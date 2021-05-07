fac329ff206dca68bef2a039dace475a8c0da182
sherbold <herbold@cs.uni-goettingen.de>
2021-05-07 17:22:29 +0200

    scripts for running python approaches

---
+++ approaches/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+py_approaches="baseline_rf_wp baseline_rf_all baseline_none baseline_all"
+
+# Iterate the string variable using for loop
+for py_approach in $py_approaches; do
+    cd $py_approach
+    source venv/bin/activate
+    python approach.py ../../data ../../scores $py_approach 3 250
+    deactivate
+done
---
+++ approaches/setup_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+py_approaches="baseline_rf_wp baseline_rf_all baseline_none baseline_all"
+
+for py_approach in $py_approaches; do
+    cd $py_approach
+    echo $PWD
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+    cd ..
+done
