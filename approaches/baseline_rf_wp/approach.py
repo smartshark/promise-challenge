9cb31cbf630e296b147bfe2110c342ddbab0fab2
sherbold <herbold@cs.uni-goettingen.de>
2021-05-07 14:53:44 +0200

    first approach template without docker

---
+++ approaches/baseline_rf_wp/approach.py
@@ -0,0 +1,80 @@
+import sys
+from sklearn.ensemble import RandomForestClassifier
+
+from imblearn.over_sampling import SMOTE
+
+from utils import *
+
+def approach():
+    args = sys.argv
+    print(args)
+    #args = ['foo.py', '../../data', '../../scores', 'baseline_rf_wp', '3', '250']
+
+    data_path = args[1]
+    score_path = args[2]
+    approach_name = args[3]
+    drop_months_end = int(args[4])
+    num_test_commits = int(args[5])
+
+    projects = load_all_projects(path=data_path)
+
+    for project in projects:
+        print(project)
+
+        train_df, test_df = prepare_within_project_data(projects[project], drop_months_end=drop_months_end, num_test_commits=num_test_commits)
+
+        #########################################
+        # Build Classifier                      #
+        # (should be adopted for your approach) #
+        #########################################
+
+        # extract columns with feature values from available data
+        # we prepared the following feature lists for your convenience:
+        #
+        # ALL_FEATURES
+        # STATIC_FEATURES
+        # STATIC_FILE_FEATURES
+        # STATIC_CLASS_FEATURES
+        # STATIC_INTERFACE_FEATURES
+        # STATIC_ENUM_FEATURES
+        # STATIC_METHOD_FEATURES
+        # FGJIT_FEATURES
+        # JIT_FEATURES
+        # WD_FEATURES
+        # PMD_FEATURES
+        #
+        # please check the documentation to see which features are included in each list
+        # TODO add url
+        # 
+        # we use all available features for our baseline
+        X_train = train_df[ALL_FEATURES].values
+        X_test = test_df[ALL_FEATURES].values
+        X_train = train_df[JIT_FEATURES].values
+        X_test = test_df[JIT_FEATURES].values
+
+        # binary labels are in the column 'is_inducing'
+        y_train = train_df['is_inducing']
+        y_test = test_df['is_inducing']
+
+        # we recommend using a fixed random seed for reproducibility, but this is up to you
+        RANDOM_SEED = 42
+        np.random.seed(RANDOM_SEED)
+
+        # we resample with SMOTE and build a random forest for our baseline
+        X_res, y_res = SMOTE(random_state=RANDOM_SEED).fit_resample(X_train, y_train)
+        rf = RandomForestClassifier()
+        rf.fit(X_res, y_res)
+        y_pred = rf.predict(X_test)
+
+        ######################################################
+        # DO NOT TOUCH FROM HERE                             #
+        # This is where the scores are calculated and stored #
+        ######################################################
+
+        scores = score_model(test_df, y_pred)
+        print_summary(train_df, test_df, scores)
+        write_scores(score_path, approach_name, project, scores)
+
+        
+if __name__ == '__main__':
+    approach()  # Or whatever function produces output
