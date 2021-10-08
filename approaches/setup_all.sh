58c863abb9608c13863a70c8244f930fed96418e
Sina <stavakkoli1999@gmail.com>
2021-10-08 12:27:07 +0330

    feat: setup file modified for svm

--- approaches/setup_all.sh
+++ approaches/setup_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-py_approaches="baseline_rf_wp baseline_rf_all baseline_none baseline_all baseline_lr"
+py_approaches="baseline_rf_wp baseline_rf_all baseline_none baseline_all baseline_lr baseline_svm"
 
 for py_approach in $py_approaches; do
     cd $py_approach
